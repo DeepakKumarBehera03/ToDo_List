b3a1c98bd25cade48ace0d8ecc462041d39a2741
DeepakKumarBehera03 <deepakjgrt99@gmail.com>
2024-02-24 19:25:33 +0530

    Designing n-tier architecture, part-2 repositories

---
+++ lib/repositories/repository.dart
@@ -0,0 +1,17 @@
+abstract class Repository{
+  Model create();
+
+  List<Model> getAll();
+  Model get(int id);
+  void update(Model items);
+
+  void delete(Model item);
+  void clear();
+}
+
+class Model{
+  final int id;
+  final Map data;
+
+  const Model({required this.id, this.data = const {}});
+}
